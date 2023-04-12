a28bb012007d14a78199cc035e2dfc4e0b4df6f6
smail <elgoufriismail1@gmail.com>
2023-04-12 16:15:05 +0000

    test open cv

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File
+import cv2
+import io
+from PIL import Image
+import numpy as np
+import pytesseract
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/ocr")
+async def ocr(file: bytes = File(...)):
+    img = Image.open(io.BytesIO(file))
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    text = pytesseract.image_to_string(img)
+    return text
